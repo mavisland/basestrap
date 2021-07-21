7bc776c372b06546008e045032a8b53771e97d5d
Tanju Yildiz <ben@tanjuyildiz.com>
2021-07-22 02:35:31 +0300

    Add `clean` task

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,18 @@ const { src, dest, series, watch } = require("gulp");
 const autoprefixer = require("autoprefixer");
 const browserSync = require("browser-sync").create();
 const cleanCSS = require("gulp-clean-css");
+const del = require("del");
 const filter = require("gulp-filter");
 const postcss = require("gulp-postcss");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass")(require("node-sass"));
 const sourcemaps = require("gulp-sourcemaps");
+
+// Remove pre-existing content from output folders
+const cleanDist = (cb) => {
+  del.sync(["dist/"]);
+  return cb();
+};
 
 // Compile, autoprefix & minify SASS files
 function buildStyles() {
@@ -65,8 +72,11 @@ const reloadBrowser = (cb) => {
 // Watch all file changes
 const watchSource = () => {
   watch("./scss/basestrap.scss", series(buildStyles, reloadBrowser));
-  watch("./docs/index.html", reloadBrowser);
+  watch("./index.html", reloadBrowser);
 };
+
+// Clean task
+exports.clean = cleanDist;
 
 // Build task
 exports.build = buildStyles;
@@ -75,4 +85,4 @@ exports.build = buildStyles;
 exports.watch = watchSource;
 
 // Default task
-exports.default = series(this.build, serveDocs, watchSource);
+exports.default = series(cleanDist, series(this.build, serveDocs, watchSource));
