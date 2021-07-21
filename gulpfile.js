4f76877cea0d7cc763f3143ece7fa911b247c551
Tanju Yildiz <ben@tanjuyildiz.com>
2021-07-22 01:35:50 +0300

    Create gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,52 @@
-const { src, dest } = require("gulp");
-const sass = require("gulp-sass")(require("node-sass"));
-const prefixer = require("autoprefixer");
-const minifier = require("cssnano");
-const postcss = require("gulp-postcss");
+/*! basestrap */
 
+// Packages
+const { src, dest } = require("gulp");
+const autoprefixer = require("autoprefixer");
+const cleanCSS = require("gulp-clean-css");
+const filter = require("gulp-filter");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass")(require("node-sass"));
+const sourcemaps = require("gulp-sourcemaps");
+
+// Compile, autoprefix & minify SASS files
 function buildStyles() {
-  return src("./scss/*.scss")
+  return src("./scss/basestrap.scss")
+    .pipe(sourcemaps.init())
     .pipe(
       sass({
-        includePaths: "./node_modules",
         outputStyle: "expanded",
+        sourceMap: true,
       }).on("error", function (err) {
         sass.logError(err);
         this.emit("end");
       })
     )
-    .pipe(dest("./css"));
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("./dist"))
+    .pipe(filter("**/*.css"))
+    .pipe(
+      cleanCSS({
+        level: {
+          1: {
+            specialComments: 0,
+          },
+        },
+      })
+    )
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("./dist"));
 }
+
+// Build task
+exports.build = buildStyles;
+
+// Default task
+exports.default = this.build;
